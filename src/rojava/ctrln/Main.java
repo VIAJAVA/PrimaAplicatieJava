fa0c095f3c418a76f92414d4417a278fb8c87e0a
VIAJAVA <siacob43@yahoo.com>
2022-03-05 15:21:58 +0200

    modificare mesaj

--- src/rojava/ctrln/Main.java
+++ src/rojava/ctrln/Main.java
@@ -2,6 +2,6 @@ package rojava.ctrln;
 
 public class Main {
     public static void main(String[] args) {
-	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
+	    System.out.println("Invat limbajul Java si despre masina virtuala!");
     }
 }
