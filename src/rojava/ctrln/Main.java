c6e8f613320ad2746058e660c7360ca55dc55981
VIAJAVA <siacob43@yahoo.com>
2022-03-05 15:11:22 +0200

    modificare main.java - stergere comentariu

--- src/rojava/ctrln/Main.java
+++ src/rojava/ctrln/Main.java
@@ -1,8 +1,6 @@
 package rojava.ctrln;
 
 public class Main {
-
-    //JAVA_HOME
     public static void main(String[] args) {
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
