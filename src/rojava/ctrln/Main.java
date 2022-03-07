8bbe11d97225aee911018c6b07195dc0c88801d2
VIAJAVA <siacob43@yahoo.com>
2022-03-07 11:15:58 +0200

    Adaugare comentarii pe o singura linie

--- src/rojava/ctrln/Main.java
+++ src/rojava/ctrln/Main.java
@@ -1,7 +1,10 @@
 package rojava.ctrln;
 
 public class Main {
+    //Aceasta este metoda main, care porneste o aplicate Java.
     public static void main(String[] args) {
+        //Inainte de comentariu nu se poate pune text.
+        //Aici vrem sa afisam pe consola Intellij un mesaj ales de noi.
 	    System.out.println("Invat limbajul Java si despre masina virtuala!");
     }
 }
