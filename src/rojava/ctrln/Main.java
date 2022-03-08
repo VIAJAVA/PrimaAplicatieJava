bbf0b59c12e9eb0635319432a2d5e01a6139f778
VIAJAVA <siacob43@yahoo.com>
2022-03-08 15:58:28 +0200

    pachet nou

--- src/rojava/ctrln/Main.java
+++ src/rojava/ctrln/Main.java
@@ -1,5 +1,6 @@
 package rojava.ctrln;
 
+
 public class Main {
 
     /**
@@ -10,7 +11,15 @@ public class Main {
      */
 
     public static void main(String[] args) {
+if (args.length!= 4){
+    System.out.println ("USAGE: java rojava.ctrln.Main 1 2 3 4");
+    return;
+}
+    if (args[0].equals("1")){
+        System.out.println ("Ar trebui sa am un singur mesaj!");
 
+        }
 	    System.out.println("Invat limbajul Java si despre masina virtuala!");
+        rojava.misc.Main.main(args);
     }
 }
---
+++ src/rojava/misc/Main.java
@@ -0,0 +1,7 @@
+package rojava.misc;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Salut din rojava.misc.Main!");
+    }
+}
